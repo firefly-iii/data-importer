dc5e5f66582a1b65c474796b7ddaeba159d6d97f
James Cole <thegrumpydictator@gmail.com>
2021-12-02 17:44:39 +0100

    Add trusted proxy form https://github.com/firefly-iii/firefly-iii/issues/5344

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,42 +1,16 @@
 <?php
-/*
- * TrustProxies.php
- * Copyright (c) 2021 james@firefly-iii.org
- *
- * This file is part of the Firefly III Data Importer
- * (https://github.com/firefly-iii/data-importer).
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
-declare(strict_types=1);
 
 namespace App\Http\Middleware;
 
-use Fideloper\Proxy\TrustProxies as Middleware;
-use Illuminate\Contracts\Config\Repository;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
-/**
- * Class TrustProxies
- */
 class TrustProxies extends Middleware
 {
     /**
      * The trusted proxies for this application.
      *
-     * @var array|string
+     * @var array|string|null
      */
     protected $proxies;
 
@@ -45,20 +19,10 @@ class TrustProxies extends Middleware
      *
      * @var int
      */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
-
-    /**
-     * TrustProxies constructor.
-     *
-     * @param Repository $config
-     */
-    public function __construct(Repository $config)
-    {
-        $trustedProxies = (string) config('importer.trusted_proxies');
-        $this->proxies  = explode(',', $trustedProxies);
-        if ('**' === $trustedProxies) {
-            $this->proxies = '**';
-        }
-        parent::__construct($config);
-    }
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
