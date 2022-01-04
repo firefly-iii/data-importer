892e1813aa499681df63ff6cde63bbfd075a45e5
James Cole <james@firefly-iii.org>
2022-01-04 13:27:50 +0100

    Revert "Update version."
    This reverts commit 253f434d2c3c3cd4bacd6e047711530d00132c96.

--- config/importer.php
+++ config/importer.php
@@ -23,7 +23,7 @@
 declare(strict_types=1);
 
 return [
-    'version'              => '0.5.1',
+    'version'              => '0.5.0',
     'flows'                => ['nordigen', 'spectre', 'csv'],
     'flow_titles'          => [
         'csv'      => 'CSV',
