b1c297d7bdd68a839a3467e75203a9c3da11ce94
James Cole <james@firefly-iii.org>
2022-04-02 13:58:35 +0200

    Jump to 0.9.2 to fix some errors.

--- config/importer.php
+++ config/importer.php
@@ -23,7 +23,7 @@
 declare(strict_types=1);
 
 return [
-    'version'                 => '0.9.1',
+    'version'                 => '0.9.2',
     'flows'                   => ['nordigen', 'spectre', 'csv'],
     'flow_titles'             => [
         'csv'      => 'CSV',
