fe820d104c4250e58fa72f0f393e641a7ba2dbad
James Cole <james@firefly-iii.org>
2022-04-10 12:26:50 +0200

    New release.

--- config/importer.php
+++ config/importer.php
@@ -23,7 +23,7 @@
 declare(strict_types=1);
 
 return [
-    'version'                 => '0.9.6',
+    'version'                 => '0.9.7',
     'flows'                   => ['nordigen', 'spectre', 'csv'],
     'flow_titles'             => [
         'csv'      => 'CSV',
