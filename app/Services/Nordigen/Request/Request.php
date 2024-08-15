629eebe52b8d5c62c47e126b61fd618d4983fd9d
James Cole <james@firefly-iii.org>
2024-08-15 19:58:36 +0200

    Add some debug things for https://github.com/firefly-iii/firefly-iii/issues/9139

--- app/Services/Nordigen/Request/Request.php
+++ app/Services/Nordigen/Request/Request.php
@@ -32,6 +32,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\TransferException;
+use Psr\Http\Message\ResponseInterface;
 
 /**
  * Class Request
@@ -90,8 +91,8 @@ abstract class Request
             $fullUrl = sprintf('%s?%s', $fullUrl, http_build_query($this->parameters));
         }
         app('log')->debug(sprintf('authenticatedGet(%s)', $fullUrl));
-        $client  = $this->getClient();
-        $body    = null;
+        $client = $this->getClient();
+        $body   = null;
 
         try {
             $res = $client->request(
@@ -106,7 +107,7 @@ abstract class Request
                     ],
                 ]
             );
-        } catch (GuzzleException|TransferException $e) {
+        } catch (GuzzleException | TransferException $e) {
             app('log')->error(sprintf('%s: %s', get_class($e), $e->getMessage()));
 
             // crash but there is a response, log it.
@@ -121,7 +122,7 @@ abstract class Request
             }
 
             // if app can get response, parse it.
-            $json            = [];
+            $json = [];
             if (method_exists($e, 'getResponse')) {
                 $body = (string) $e->getResponse()->getBody();
                 $json = json_decode($body, true) ?? [];
@@ -139,6 +140,9 @@ abstract class Request
 
             throw $exception;
         }
+
+        $this->logRateLimitHeaders($res);
+
         if (200 !== $res->getStatusCode()) {
             // return body, class must handle this
             app('log')->error(sprintf('[1] Status code is %d', $res->getStatusCode()));
@@ -225,7 +229,7 @@ abstract class Request
             $fullUrl = sprintf('%s?%s', $fullUrl, http_build_query($this->parameters));
         }
 
-        $client  = $this->getClient();
+        $client = $this->getClient();
 
         try {
             $res = $client->request(
@@ -244,7 +248,7 @@ abstract class Request
             // TODO error response, not an exception.
             throw new ImporterHttpException(sprintf('AuthenticatedJsonPost: %s', $e->getMessage()), 0, $e);
         }
-        $body    = (string) $res->getBody();
+        $body = (string) $res->getBody();
 
         try {
             $json = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
@@ -255,4 +259,42 @@ abstract class Request
 
         return $json;
     }
+
+    private function logRateLimitHeaders(ResponseInterface $res): void
+    {
+        $ignore  = ['server', 'date', 'content-type', 'content-length', 'connection', 'vary', 'Content-Length', 'allow', 'cache-control', 'x-frame-options', 'content-language', 'x-c-uuid', 'x-u-uuid', 'x-content-type-options', 'referrer-policy', 'client-region', 'cf-ipcountry', 'strict-transport-security', 'Via', 'Alt-Svc'];
+        $headers = $res->getHeaders();
+        foreach ($headers as $header => $content) {
+            if (in_array($header, $ignore, true)) {
+                continue;
+            }
+            app('log')->debug(sprintf('Header: %s: %s', $header, $content[0] ?? '(empty)'));
+        }
+        // HTTP_X_RATELIMIT_LIMIT: Indicates the maximum number of allowed requests within the defined time window.
+        // HTTP_X_RATELIMIT_REMAINING: Shows the number of remaining requests you can make in the current time window.
+        // HTTP_X_RATELIMIT_RESET
+        if (array_key_exists('x-ratelimit-limit', $headers)) {
+            app('log')->debug(sprintf('Rate limit: %s', $headers['x-ratelimit-limit'][0]));
+        }
+        if (array_key_exists('x-ratelimit-remaining', $headers)) {
+            app('log')->debug(sprintf('Rate limit remaining: %s', $headers['x-ratelimit-remaining'][0]));
+        }
+        if (array_key_exists('x-ratelimit-reset', $headers)) {
+            app('log')->debug(sprintf('Rate limit reset: %s', $headers['x-ratelimit-reset'][0]));
+        }
+
+        // HTTP_X_RATELIMIT_ACCOUNT_SUCCESS_LIMIT: Indicates the maximum number of allowed requests within the defined time window.
+        // HTTP_X_RATELIMIT_ACCOUNT_SUCCESS_REMAINING: Shows the number of remaining requests you can make in the current time window.
+        // HTTP_X_RATELIMIT_ACCOUNT_SUCCESS_RESET: Provides the time remaining in the current window.
+        if (array_key_exists('x-ratelimit-account-success-limit', $headers)) {
+            app('log')->debug(sprintf('Account success rate limit: %s', $headers['x-ratelimit-limit'][0]));
+        }
+        if (array_key_exists('x-ratelimit-account-success-remaining', $headers)) {
+            app('log')->debug(sprintf('Account success rate limit remaining: %s', $headers['x-ratelimit-remaining'][0]));
+        }
+        if (array_key_exists('x-ratelimit-account-success-reset', $headers)) {
+            app('log')->debug(sprintf('Account success rate limit reset: %s', $headers['x-ratelimit-reset'][0]));
+        }
+    }
+
 }
