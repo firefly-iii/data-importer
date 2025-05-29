e76491699c333f1cd8f8089a18a556cf3688f7af
James Cole <james@firefly-iii.org>
2025-05-29 20:16:58 +0200

    Add AccountNumber cleaner that removes spaces from account numbers, normalizing them for Firefly III use.

---
+++ app/Services/CSV/Converter/AccountNumber.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services\CSV\Converter;
+
+use App\Support\Facades\Steam;
+
+class AccountNumber implements ConverterInterface
+{
+
+    public function convert($value)
+    {
+        $value = (string)$value;
+
+        // replace spaces from cleaned string.
+        return str_replace("\x20", '', Steam::cleanStringAndNewlines($value));
+    }
+
+    public function setConfiguration(string $configuration): void
+    {
+    }
+}
