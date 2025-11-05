e45aed8dc90db6dd0fd4b14e927f42afef264684
Beat Stieger <beat.stieger@xlease.ch>
2025-11-05 05:13:58 +0100

    Use new TransactionFactory to crate and we might also keep the $configuration for further use :D

--- app/Services/Camt/Conversion/TransactionExtractor.php
+++ app/Services/Camt/Conversion/TransactionExtractor.php
@@ -25,6 +25,7 @@ declare(strict_types=1);
 namespace App\Services\Camt\Conversion;
 
 use App\Services\Camt\Transaction;
+use App\Services\Camt\TransactionFactory;
 use App\Services\Shared\Configuration\Configuration;
 use App\Services\Shared\Conversion\ProgressInformation;
 use Genkgo\Camt\Camt053\DTO\Statement as CamtStatement;
@@ -38,6 +39,7 @@ class TransactionExtractor
     public function __construct(private Configuration $configuration)
     {
         Log::debug(sprintf('[%s] Now in %s', config('importer.version'), __METHOD__));
+        $this->configuration = $configuration;
     }
 
     public function extractTransactions(Message $message): array
@@ -54,6 +56,7 @@ class TransactionExtractor
          */
         foreach ($statements as $i => $statement) { // -> Level B
             $entries    = $statement->getEntries();
+            $camtType   = $this->configuration->getCamtType();
             $entryCount = count($entries);
             Log::debug(sprintf('[%s] [%d/%d] Now working on statement with %d entries.', config('importer.version'), $i + 1, $totalCount, $entryCount));
             foreach ($entries as $ii => $entry) {                // -> Level C
@@ -61,24 +64,24 @@ class TransactionExtractor
                 Log::debug(sprintf('[%s] [%d/%d] Now working on entry with %d detail entries.', config('importer.version'), $ii + 1, $entryCount, $count));
                 if (0 === $count) {
                     // TODO Create a single transaction, I guess?
-                    $transactions[] = new Transaction($message, $statement, $entry, []);
+                    $transactions[] = TransactionFactory::create($camtType, $message, $statement, $entry, []);
                 }
                 if (0 !== $count) {
                     $handling = $this->configuration->getGroupedTransactionHandling();
                     if ('split' === $handling) {
-                        $transactions[] = new Transaction($message, $statement, $entry, $entry->getTransactionDetails());
+                        $transactions[] = TransactionFactory::create($camtType, $message, $statement, $entry, $entry->getTransactionDetails());
                     }
                     if ('single' === $handling) {
                         foreach ($entry->getTransactionDetails() as $detail) {
-                            $transactions[] = new Transaction($message, $statement, $entry, [$detail]);
+                            $transactions[] = TransactionFactory::create($camtType, $message, $statement, $entry, [$detail]);
                         }
                     }
                     if ('group' === $handling) {
                         if (1 === $count) {
-                            $transactions[] = new Transaction($message, $statement, $entry, $entry->getTransactionDetails());
+                            $transactions[] = TransactionFactory::create($camtType, $message, $statement, $entry, $entry->getTransactionDetails());
                         }
                         if ($count > 1) {
-                            $transactions[] = new Transaction($message, $statement, $entry, []);
+                            $transactions[] = TransactionFactory::create($camtType, $message, $statement, $entry, []);
                         }
                     }
                 }
