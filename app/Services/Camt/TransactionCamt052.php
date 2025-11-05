ec488f69106859c279437ec3f2efec099cbf69c0
Beat Stieger <beat.stieger@xlease.ch>
2025-11-05 05:07:02 +0100

    Add Factory to crate Object depending on Camt-Type. Also add concrete classes for each type (even with no individual logic). Put old "Transaction" to "AbstractTransaction"

---
+++ app/Services/Camt/TransactionCamt052.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Camt;
+
+use Genkgo\Camt\Camt052\DTO\Report;
+use Genkgo\Camt\DTO\Entry;
+
+class TransactionCamt052 extends AbstractTransaction
+{
+    public function __construct(
+        protected readonly \Genkgo\Camt\DTO\Message $levelA,
+        protected readonly Report $levelB,
+        protected readonly Entry $levelC,
+        protected array $levelD
+    ) {
+        Log::debug('Constructed a CAMT.052 Transaction');
+    }
+
+    /*public function getFieldByIndex(string $field, int $index): string
+    {
+        // implement 053-specific Logic
+        return '';
+    }*/
+}
---
+++ app/Services/Camt/TransactionFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Camt;
+
+use Genkgo\Camt\DTO\Message;
+use Genkgo\Camt\Camt052\DTO\Statement as Statement052;
+use Genkgo\Camt\Camt053\DTO\Statement as Statement053;
+use Genkgo\Camt\DTO\Entry;
+
+
+class TransactionFactory
+{
+    public static function create(string $camtType, Message $msg, object $levelB, Entry $entry, array $splits): AbstractTransaction
+    {
+        // Read Config heres        
+
+        if ('052' == $camtType) {
+            return new TransactionCamt052($msg, $levelB, $entry, $splits);
+        }
+
+        if ('053' == $camtType) {
+            return new TransactionCamt053($msg, $levelB, $entry, $splits);
+        }
+        // TODO what to do in case of "no valid camtType"?
+        return new TransactionCamt053();
+    }
+}
